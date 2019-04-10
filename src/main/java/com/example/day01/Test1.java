f27c5821da31539821840582a7b01211207c8558
mady <dongyang.ma@e-dewin.com>
2019-04-10 18:45:20 +0800

    test

---
+++ src/main/java/com/example/day01/Test1.java
@@ -0,0 +1,24 @@
+/**
+ * Project: demo
+ * <p>
+ * File Created at 2019/4/10
+ * <p>
+ * Copyright 2019 e-dewin.com
+ * Corporation Limited.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * dewin Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with e-dewin.com
+ */
+package com.example.day01;
+
+/**
+ * @author: mady
+ * @version: 1.0
+ * @date: 2019/4/10 18:44
+ */
+public class Test1 {
+}
